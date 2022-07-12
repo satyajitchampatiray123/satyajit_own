ce41fbc1ce1b746c6bbe251f9f252da37602c142
Satyajit <satyajit_cs@yahoo.co.in>
2022-07-12 23:51:09 +0530

    dfd

---
+++ SpringBootSampleFirst/src/main/java/com/own/example/SpringBootSampleFirstApplication.java
@@ -0,0 +1,13 @@
+package com.own.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSampleFirstApplication.class, args);
+	}
+
+}
